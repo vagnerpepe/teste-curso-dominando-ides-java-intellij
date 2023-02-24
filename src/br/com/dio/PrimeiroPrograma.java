7ccdfe8ebfe708a426566549e78aff618f9b7369
Vagner <vagnercontabilidade@bol.com.br>
2023-02-24 20:25:50 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,8 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
-        /*int a = 5;
+        int a = 5;
         int b = 3;
-        System.out.println("Hello World! " + (a+b));*/
+        System.out.println("Hello World! " + (a+b));
     }
 }
